3cbd5d6409135ee37316e2774d811fe7fde364ab
VtrPedrollo <pedrollo.vitor@gmail.com>
2024-02-09 23:17:40 -0300

    minimax_custom usando 3 heuristicas mescladas

--- advsearch/headkicker/othello_minimax_custom.py
+++ advsearch/headkicker/othello_minimax_custom.py
@@ -11,6 +11,17 @@ from .minimax import minimax_move
 # do seu agente.
 
 
+EVAL_TEMPLATE = [
+    [100, -30, 6, 2, 2, 6, -30, 100],
+    [-30, -50, 1, 1, 1, 1, -50, -30],
+    [  6,   1, 1, 1, 1, 1,   1,   6],
+    [  2,   1, 1, 3, 3, 1,   1,   2],
+    [  2,   1, 1, 3, 3, 1,   1,   2],
+    [  6,   1, 1, 1, 1, 1,   1,   6],
+    [-30, -50, 1, 1, 1, 1, -50, -30],
+    [100, -30, 6, 2, 2, 6, -30, 100]
+]
+
 def make_move(state) -> Tuple[int, int]:
     """
     Returns a move for the given game state
@@ -34,12 +45,39 @@ def evaluate_custom(state, player:str) -> float:
     :param state: state to evaluate (instance of GameState)
     :param player: player to evaluate the state for (B or W)
     """
-    
-    # Heurística: quantidade de jogadas legais
-    # Inspirada em: http://www.cs.cornell.edu/~yuli/othello/othello.html
-
-    player_legal_moves = len(state.board.legal_moves(player))
+    board = state.board.tiles
     opponent = Board.opponent(player)
+    player_score = 0
+    opponent_score = 0
+    for row_idx, row in enumerate(board):
+        for (col_idx, node) in enumerate(row):
+            score = EVAL_TEMPLATE[row_idx][col_idx]
+            if (node == player):
+                player_score += score
+            elif (node == opponent):
+                opponent_score += score
+
+    weighted_score = player_score - opponent_score
+
+    # Diferença de Peças: Diferença entre o número de peças do jogador e do oponente
+    player_pieces = sum(1 for x in range(8) for y in range(8) if board[x][y] == player)
+    opponent_pieces = sum(1 for x in range(8) for y in range(8) if board[x][y] == opponent)
+    piece_difference_score = player_pieces - opponent_pieces
+
+    # Mobilidade: Diferença entre o número de movimentos legais do jogador e do oponente
+    player_legal_moves = len(state.board.legal_moves(player))
     opponent_legal_moves = len(state.board.legal_moves(opponent))
-    score = player_legal_moves - opponent_legal_moves
-    return score
+    mobility_score = player_legal_moves - opponent_legal_moves
+
+    # Pesos: Ajuste esses valores conforme necessário
+    piece_difference_weight = 0.3
+    mobility_weight = 0.7
+    weighted_score_weight = 1
+
+    # Calculando a pontuação final
+    evaluation = (piece_difference_weight * piece_difference_score +
+                   mobility_weight * mobility_score +
+                   weighted_score_weight * weighted_score)
+
+    return evaluation
+
